ba5786084c394bcf1a548ef5a1ce4b4b5956726c
martynas_a <martynas@interactio.io>
2022-12-27 13:41:46 +0200

    added base page object

---
+++ test/pageobjects/base.page.ts
@@ -0,0 +1,13 @@
+/**
+ * main page object containing all methods, selectors and functionality
+ * that is shared across all the page objects
+ */
+export default class BasePage {
+    /**
+     * Opens a base page or optionally a sub-page of the page
+     * @param subPath path of the sub-page
+     */
+    protected openPath(subPath?: string): Promise<string> {
+        return browser.url(`https://nordpass.com/${subPath}`);
+    }
+}
