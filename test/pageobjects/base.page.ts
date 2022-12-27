d85b4da1ad39f8bd97e50424b377ee7283ea4410
martynas_a <martynas@interactio.io>
2022-12-27 16:59:07 +0200

    added selector to get all of HTML content

--- test/pageobjects/base.page.ts
+++ test/pageobjects/base.page.ts
@@ -3,11 +3,15 @@
  * that is shared across all the page objects
  */
 export default class BasePage {
+    protected get htmlRoot() {
+        return $('*');
+    }
+
     /**
      * Opens a base page or optionally a sub-page of the page
      * @param subPath path of the sub-page
      */
-    protected openPath(subPath?: string): Promise<string> {
+    protected openPath(subPath: string = ''): Promise<string> {
         return browser.url(`https://nordpass.com/${subPath}`);
     }
 }
